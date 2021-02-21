ea1de38d10671e65c599b4edc65b6f9c825fa9eb
Aaron Lei <aaron.l@live.ca>
2021-02-21 14:38:59 -0500

    added DonutChart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import NavBar from "./components/NavBar/NavBar";
 import SideBar from "./components/SideBar/SideBar";
+
+import "./App.css";
 
 function App() {
   const [data, setData] = useState(null);
@@ -19,6 +24,17 @@ function App() {
     <div className="App">
       <NavBar />
       <SideBar />
+      <div className="main-content">
+        <div className="heading">
+          <h1>Performance Management</h1>
+        </div>
+        <div className="banner">
+          <header className="banner-content">
+            <FontAwesomeIcon icon={faGlobe} />
+            <h2>Diagnositic Tool</h2>
+          </header>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/DonutChart/DonutChart.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from "react";
+import * as d3 from "d3";
+
+function DonutChart({ id, data, selected, onClick }) {
+  useEffect(() => {
+    d3.select(`#donut-chart-${id} svg`).remove();
+
+    const svg = d3
+      .select(`#donut-chart-${id}`)
+      .append("svg")
+      .attr("width", 100)
+      .attr("height", 100)
+      .style("position", "relative");
+
+    const width = svg.attr("width");
+    const height = svg.attr("height");
+
+    const g = svg
+      .append("g")
+      .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+    const colors = d3.scaleOrdinal([
+      "var(--color-gray)",
+      selected ? "var(--color-highlight)" : "var(--color-primary-main)",
+    ]);
+
+    const values = [100 - data.score, data.score];
+
+    const pie = d3.pie().value((d) => d);
+
+    const arc = d3.arc().innerRadius(42).outerRadius(50);
+
+    const arcs = g
+      .selectAll(".arc")
+      .data(pie(values))
+      .enter()
+      .append("g")
+      .attr("class", "arc");
+
+    arcs
+      .append("path")
+      .attr("d", arc)
+      .attr("fill", (d) => colors(d.data));
+  }, [selected, id, data]);
+  return (
+    <div
+      id={`donut-chart-${id}`}
+      className="donut-chart"
+      onClick={onClick}
+      style={
+        selected
+          ? {
+              borderColor: "var(--color-highlight)",
+              backgroundColor: "var(--color-highlight-bg",
+            }
+          : null
+      }
+    >
+      <div className="gaugeData-content">
+        <div
+          className="name"
+          style={selected ? { color: "var(--color-highlight)" } : null}
+        >
+          {data.name}
+        </div>
+        <svg />
+        <div
+          className="score"
+          style={selected ? { color: "var(--color-highlight)" } : null}
+        >
+          {data.score}%
+        </div>
+        <div className="vsly">{data.vsly}</div>
+        <div className="sample">Sample: {data.sample}</div>
+      </div>
+    </div>
+  );
+}
+
+export default DonutChart;
